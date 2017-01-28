2883e15119df314edff3cbfee4fb2e1536b737e2
khannagautam <khannagautam96@gmail.com>
2017-01-28 16:36:06 +0530

    Add files via upload
    Source code

---
+++ louvain.cpp
@@ -0,0 +1,233 @@
+/* Implementation 2 of BiLouvain Method dated 28.01.2017
+   Author: Gautam Khanna  */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+//defining a community
+struct cluster
+{
+	vector<int> vert;
+	//list of vertices in the community
+	int blue,red,self;
+	//to store the degree of red and blue and self loops of the community
+	bool single;
+};
+
+/*Input format : 1. Enter number of vertices and edges
+				 2. Enter the colour of n vertices. 1- red, 2-blue
+				 3. Enter the edge list
+				 
+*/
+
+/* Please note that the code assumes all the vertices in the edge list should lie between 1 and n*/
+
+int main()
+{
+	freopen("in.txt","r",stdin);
+	freopen("out.txt","w",stdout);
+	int n,m,i,j,a,b;
+	cin>>n>>m;
+	
+	
+	//n- number of vertices in the graph
+	//m- the number of edges in the graph
+	
+	
+	map<int, map<int,pair<int,int> > >adj;//map of map of pairs to store the adjacency list
+	//first: red edges from i-j, second: blue edges from i-j
+	
+	map<int,cluster> track;//map to store the active clusters
+	
+	vector<int> parent(n+1);//vector storing the id which is the supernode of that cluster
+	
+	vector<int> colour(n+1);
+	int c=0;//stores the colour entered by user
+	
+	/* Taking the colours of the n vertices as input
+	   1 if it is red, 0 otherwise*/
+	/*creating and initializing n communities*/
+	for(i=1;i<=n;i++)
+	{
+		cin>>c;
+		colour[i]=c;
+		cluster cl;
+		cl.vert.resize(0);
+		cl.red=cl.blue=0;
+		
+		parent[i]=i;// initial parent is the node itself
+		cl.red=cl.blue=cl.self=0;  
+		cl.vert.push_back(i);
+		cl.single=true;
+		track[i]=cl;//storing the community
+	}
+	cluster c1,c2;
+	//taking the edge list as input
+	for(i=0;i<m;i++)
+	{
+		cin>>a>>b;
+		c1=track[a];
+		c2=track[b];
+		if(colour[a])
+		{
+			adj[a][b].first++;
+			adj[b][a].second++;
+			c1.red++;
+			c2.blue++;
+		}
+		else
+		{
+			
+			adj[a][b].second++;
+			adj[b][a].first++;
+			c2.red++;
+			c1.blue++;
+		}
+	//	cout<<a<<" "<<b<<" "<<c1.red<<" "<<c1.blue<<" "<<c2.red<<" "<<c2.blue<<" "<<adj[a][b].first<<" "<<adj[a][b].second<<endl;
+			track[a]=c1;
+			track[b]=c2;
+	}
+	
+	bool flag=true;//flag to indicate a positive value of del Q for each pass
+	while(flag)
+	{
+		
+		flag=false;//set flag as false for each pass
+		for(map<int,map<int,pair<int,int> > >::iterator it=adj.begin();it!=adj.end();it++)
+		{
+			
+			i=it->first;//index of the cluster we are on
+			map<int,int> done;//indicates the clusters which have been visited for a particular index
+			double maxmod=-1;//maximum modularity initialized with -1
+			int pos=-1;//position where maximum modularity is obtained
+			double mod;//modularity for a neighbour
+			cluster c1=track[i];//present community
+		
+			/* Going through all the neighbours of cluster i and finding the modularity value*/		
+			for(map<int,pair<int,int> >::iterator j=adj[i].begin();j!=adj[i].end();j++)
+			{
+				//if neighbour part of the same cluster
+				if(parent[j->first]==i) 
+			 		continue;
+			 		
+			 	//if neighbour's community has already been visited
+				if(done[parent[j->first]]>0)
+			 		continue;
+			
+				
+				int p=j->first;
+				while(parent[p]!=p)
+				 p=parent[p];
+			 
+			 	//p- parent id of the community
+				done[p]++;//updating the done map so that this cluster is not repeated again
+				
+				int deg= adj[i][p].first+adj[i][p].second;
+				//deg= weight of edge between i and p
+				
+				cluster c2;
+				c2=track[p];
+				//neighbouring community
+				
+				if(c1.single && c2.single)
+				{
+					//merge singletons. CASE 1
+					if(c1.red==0)
+					{
+						//vertex is blue
+						mod= m-(c1.blue*c2.red);
+						mod/=(m*m);
+					}	
+					else
+					{
+						//vertex is red
+						mod= m-(c2.blue*c1.red);
+						mod*=1.0;
+						mod/=(m*m);
+					}
+				
+				}	
+				else if(c1.single)
+				{
+					//singleton and cluster. CASE 2a
+					if(c1.blue==0)
+					{
+						mod= (deg*1.0/m)-((c1.red*1.0*c2.blue)/(m*m));
+					}
+					else
+					{
+						mod=(deg*1.0/m)-((c2.red*1.0*c1.blue)/(m*m));
+					}
+				}	
+				else if(c2.single)
+				{
+					//singleton and cluster. CASE 2b
+					if(c2.red==0)
+					{
+						mod= (deg*1.0/m)-((c1.red*1.0*c2.blue)/(m*m));
+					}
+					else
+					{
+						mod=(deg*1.0/m)-((c2.red*1.0*c1.blue)/(m*m));
+					}
+				}
+				else
+				{
+					//cluster and cluster. CASE 3
+					mod= (deg*1.0/m)-((c1.red*1.0*c2.blue)/(m*m))-((c2.red*1.0*c1.blue)/(m*m));
+				
+				}
+			
+				if(mod>maxmod)
+				{
+					maxmod=mod;
+					pos=p;
+				}
+			
+		}
+	//	cout<<i<<" "<<pos<<" "<<maxmod<<" "<<endl;
+	
+		/* if maximum possible del Q obtained is positive merging the two clusters*/
+		if(maxmod>0 && i!=pos)
+		{
+			flag=true;//shows work is done in this pass
+			cluster c2=track[pos];
+			//updating cluster 1
+			c1.single=false;
+			c1.red+=c2.red-adj[i][pos].first;
+			c1.blue+=c2.blue-adj[i][pos].second;
+			c1.self+=c2.self+adj[i][pos].first+adj[i][pos].second;
+			parent[pos]=i;
+			for(int kk=0;kk<c2.vert.size();kk++)
+			{
+				c1.vert.push_back(c2.vert[kk]);
+			}
+			//updating adjacency list: weight of edges from new cluster will include the previous weights of cluster 2
+			for(map<int,pair<int,int> >:: iterator kk=adj[pos].begin();kk!=adj[pos].end();kk++)
+			{
+				adj[i][kk->first].first+=kk->second.first;
+				adj[i][kk->first].second+=kk->second.second;
+			}
+			track[i]=c1;
+			adj.erase(pos);
+			track.erase(pos);
+			
+		}
+	}
+	
+	
+ }
+ 
+ /*printing the vertices present in the final clusters after the algo has run, line by line*/
+ 	for(map<int,cluster >::iterator it=track.begin();it!=track.end();it++)
+	{
+		i=it->first;
+		cluster c=track[i];
+		for(j=0;j<c.vert.size();j++)
+		  cout<<c.vert[j]<<" ";
+		cout<<endl;
+	}
+
+}
+	
+
