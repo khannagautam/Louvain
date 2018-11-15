fed3d33af794f61b4cb5f47533ecab38c1507223
Mohit <mohitbaid122@gmail.com>
2018-11-15 14:23:49 +0530

    Lovain Code Updated

--- louvain.cpp
+++ louvain.cpp
@@ -5,9 +5,6 @@
 /*Input format : 1. Enter number of vertices and edges
 		 2. Enter the edge list(0-indexed)
 		 3. 1 if you want the code to read the label names from "label.txt", 0 otherwise
-
-
-
 */
 
 #include<bits/stdc++.h>
@@ -80,7 +77,7 @@ int getParent(int v,vector<int>& parent)
 
 int main()
 {
-	//freopen("in.txt","r",stdin);
+	freopen("edge_list.txt","r",stdin);
 	//freopen("out.txt","w",stdout);
 	int n,m,i,j,a,b,fl,w;
 	cin>>n>>m;
@@ -90,15 +87,16 @@ int main()
 	//m- the number of edges in the graph
 
 
-	unordered_map<int, unordered_map<int,pair<int,int> > >adj;//unordered_map of unordered_map of pairs to store the adjacency list
+	map<int, map<int,pair<int,int> > >adj;//unordered_map of unordered_map of pairs to store the adjacency list
 	//first: red edges from i-j, second: blue edges from i-j
 
-	unordered_map<int,cluster> track;//unordered_map to store the active clusters
+	map<int,cluster> track;//unordered_map to store the active clusters
 
 	vector<int> parent(n);//vector storing the id which is the supernode of that cluster
 	string label[n];		//for cluster labels
-	cout << "\nn = " << n << endl;
-	vector<int> colour(n);
+	//cout << "\nn = " << n << endl;
+	vector<int> colour(n,-1);
+	vector<int> neigh(n,-1);
 	// int c=0;//stores the colour entered by user
 
 	/*creating and initializing n communities*/
@@ -107,8 +105,6 @@ int main()
 		colour[i]=-1;
 		cluster cl;
 		cl.vert.resize(0);
-		cl.red=cl.blue=0;
-
 		parent[i]=i;// initial parent is the node itself
 		cl.red=cl.blue=cl.self=0;
 		cl.vert.push_back(i);
@@ -122,15 +118,15 @@ int main()
 	vector<int>weights(m,0);
 	for(i=0;i<m;i++)
 	{
-		cin>>a>>b>>w;
+		cin>>a>>b;//>>w;
 		edge_list[a].push_back(b);
 		edge_list[b].push_back(a);
-		weights[i]=w;
+		weights[i]=1;
 		in.push_back(make_pair(a,b));
-		cout<<a<<" "<<endl;
+		//cout<<a<<" "<<endl;
 	}
 	bfs(colour,edge_list,n);
-	
+	for(i=0;i<n;i++)	cout<<colour[i]<<" ";	cout<<endl;
 	
 	for(i=0;i<m;i++)
 	{
@@ -163,7 +159,7 @@ int main()
 
 	// cout<<"Enter 1 to give node labels, else press any other number\n";
 	// cin>>fl;	// 1 To indicate node labels available 
-	fl = 1;
+	fl = 0;
 	if(fl==1)		
 	{
 			freopen("label.txt","r",stdin);
@@ -175,25 +171,26 @@ int main()
 	}
 
 
-
 	cout<<"Pass 0: "<<modularity<<endl;
+	vector<double> maxmod(n);
 	bool flag=true;//flag to indicate a positive value of del Q for each pass
-	while(flag)
+	while(true)
 	{
-
+		cerr<<"jijij\n";
 		flag=false;//set flag as false for each pass
-		for(unordered_map<int,unordered_map<int,pair<int,int> > >::iterator it=adj.begin();it!=adj.end();it++)
+		double modu=0;
+		for(map<int,map<int,pair<int,int> > >::iterator it=adj.begin();it!=adj.end();it++)
 		{
 
 			i=it->first;//index of the cluster we are on
-			unordered_map<int,int> done;//indicates the clusters which have been visited for a particular index
-			double maxmod=-1;//maximum modularity initialized with -1
+			//unordered_map<int,int> done;//indicates the clusters which have been visited for a particular index
+			maxmod[i]=-1;//maximum modularity initialized with -1
 			int pos=-1;//position where maximum modularity is obtained
 			double mod;//modularity for a neighbour
 			cluster c1=track[i];//present community
-
+			neigh[i]=i;
 			/* Going through all the neighbours of cluster i and finding the modularity value*/
-			for(unordered_map<int,pair<int,int> >::iterator j=adj[i].begin();j!=adj[i].end();j++)
+			for(map<int,pair<int,int> >::iterator j=adj[i].begin();j!=adj[i].end();j++)
 			{
 
 				int p=getParent(j->first,parent);
@@ -209,11 +206,11 @@ int main()
 			// cerr<<"Red degree: "<<c2.red<<"\n Blue degree: "<<c2.blue<<"\n";
 			// cerr<<"Red* Blue= "<<c1.red*c2.blue<<"\n edge sqaure "<<m*m<<"\n";
 			 	//if neighbour's community has already been visited
-				if(done[p]>0)
-			 		continue;
+			//	if(done[p]>0)
+			 //		continue;
 
 
-			 	done[p]++;//updating the done unordered_map so that this cluster is not repeated again
+			 //	done[p]++;//updating the done unordered_map so that this cluster is not repeated again
 
 				int deg= adj[i][p].first+adj[i][p].second;
 				//deg= weight of edge between i and p
@@ -221,27 +218,23 @@ int main()
 				cluster c2;
 				c2=track[p];
 				//neighbouring community
-				long long pro;
+				int pro;
 				if(c1.single && c2.single)
 				{
 					//merge singletons. CASE 1
 					if(c1.red==0)
 					{
 						//vertex is blue
-						pro=c1.blue*1LL*c2.red;
-						mod= m-(pro);
-						mod*=1.0;
-						mod/=m;
-						mod/=m;
+						mod= c1.blue*1.0/m;
+						mod *= c2.red*1.0/m;
+						mod=(1.0/m)-mod;
 					}
 					else
 					{
 						//vertex is red
-						pro=c2.blue*1LL*c1.red;
-						mod= m-(pro);
-						mod*=1.0;
-						mod/=m;
-						mod/=m;
+						mod= c2.blue*1.0/m;
+						mod *= c1.red*1.0/m;
+						mod=(1.0/m)-mod;
 
 					}
 
@@ -251,100 +244,119 @@ int main()
 					//singleton and cluster. CASE 2a
 					if(c1.blue==0)
 					{
-						pro=c1.red*1LL*c2.blue;
-						double div=pro*1.0/m;
-						div/=m;
-						mod= (deg*1.0/m)-div;
+						mod= c2.blue*1.0/m;
+						mod *= c1.red*1.0/m;
+						mod= (deg*1.0/m)-mod;
 					}
 					else
 					{
 						//mod=(deg*1.0/m)-((c2.red*1.0*c1.blue)/(m*m));
-						pro=c2.red*1LL*c1.blue;
-						double div=pro*1.0/m;
-						div/=m;
-						mod= (deg*1.0/m)-div;
+						mod= c1.blue*1.0/m;
+						mod *= c2.red*1.0/m;
+						mod= (deg*1.0/m)-mod;					
 					}
-				}
+				}	
 				else if(c2.single)
 				{
 					//singleton and cluster. CASE 2b
 					if(c2.red==0)
 					{
-						pro=c1.red*1LL*c2.blue;
-						double div=pro*1.0/m;
-						div/=m;
-						mod= (deg*1.0/m)-div;
+						mod= c2.blue*1.0/m;
+						mod *= c1.red*1.0/m;
+						mod= (deg*1.0/m)-mod;
 						//mod= (deg*1.0/m)-((c1.red*1.0*c2.blue)/(m*m));
 					}
 					else
 					{
-						pro=c2.red*1LL*c1.blue;
-						double div=pro*1.0/m;
-						div/=m;
-						mod= (deg*1.0/m)-div;
+						mod= c1.blue*1.0/m;
+						mod *= c2.red*1.0/m;
+						mod= (deg*1.0/m)-mod;	
 						//mod=(deg*1.0/m)-((c2.red*1.0*c1.blue)/(m*m));
 					}
 				}
 				else
 				{
 					//cluster and cluster. CASE 3
-					long long pro1=c1.red*1LL*c2.blue;
-					pro=c2.red*1LL*c1.blue;
-					double div1=pro*1.0/m;
-					div1/=m;
-					double div2=pro1*1.0/m;
-					div2/=m;
-					mod= (deg*1.0/m)-div1-div2;
+					double div1;
+					mod= c2.blue*1.0/m;
+					mod *= c1.red*1.0/m;
+					
+					div1= c1.blue*1.0/m;
+					div1 *= c2.red*1.0/m;
+					
+					mod= (deg*1.0/m)-div1-mod;
 					//mod= (deg*1.0/m)-((c1.red*1.0*c2.blue)/(m*m))-((c2.red*1.0*c1.blue)/(m*m));
 
 				}
 
-				if(mod>maxmod)
+				if(mod>maxmod[i])
 				{
-					maxmod=mod;
+					maxmod[i]=mod;
 					pos=p;
+					neigh[i] = p;
 				}
 
+			}
 		}
+		
 	//	cout<<i<<" "<<pos<<" "<<maxmod<<" "<<endl;
+//		for(i=0;i<n;i++)	cerr<<neigh[i]<<" "<<endl;	cout<<endl;		
+		//vector<bool> visited(n,false);
+		vector<bool> rem(n,false);
+		modu=0;		
+		for(i=0;i<n;i++)
+		{			
+			if(adj.find(i) == adj.end())		continue;
+			int pari = getParent(i,parent);		
+			int parj = getParent(neigh[i],parent);
+			//if(pari == parj)			continue;
+			if(maxmod[i]>0.000001)
+				{
+					int pos=neigh[i];					
+					modu+=maxmod[i];
+					cluster c1 = track[pari];
+					cluster c2=track[parj];
+					//updating cluster 1
+					
+					c1.single=false;
+					c1.red+=c2.red-adj[i][pos].first;
+					c1.blue+=c2.blue-adj[i][pos].second;
+					c1.self+=c2.self+adj[i][pos].first+adj[i][pos].second;
+					//cerr<<"merging "<<pari<<" and "<<parj<<endl;
+					//cerr<<c1.red<<" "<<c1.blue<<" "<<c1.self<<endl;					
+					parent[parj]=i;
 
-		/* if maximum possible del Q obtained is positive merging the two clusters*/
-		if(maxmod>0.000001 && i!=pos)
-		{
-			flag=true;//shows work is done in this pass
-			modularity+=maxmod;
-			cluster c2=track[pos];
-			//updating cluster 1
-			c1.single=false;
-			c1.red+=c2.red-adj[i][pos].first;
-			c1.blue+=c2.blue-adj[i][pos].second;
-			c1.self+=c2.self+adj[i][pos].first+adj[i][pos].second;
-			parent[pos]=i;
-			for(int kk=0;kk<c2.vert.size();kk++)
-			{
-				c1.vert.push_back(c2.vert[kk]);
-			}
-			//updating adjacency list: weight of edges from new cluster will include the previous weights of cluster 2
-			for(unordered_map<int,pair<int,int> >:: iterator kk=adj[pos].begin();kk!=adj[pos].end();kk++)
-			{
-				adj[i][kk->first].first+=kk->second.first;
-				adj[i][kk->first].second+=kk->second.second;
-			}
-			track[i]=c1;
-			adj.erase(pos);
-			track.erase(pos);
+					for(int kk=0;kk<c2.vert.size();kk++)
+					{
+						c1.vert.push_back(c2.vert[kk]);
+					}
 
+					//updating adjacency list: weight of edges from new cluster will include the previous weights ofcluster2
+					for(map<int,pair<int,int> >:: iterator kk=adj[parj].begin();kk!=adj[parj].end();kk++)
+					{
+						adj[pari][kk->first].first+=kk->second.first;
+						adj[pari][kk->first].second+=kk->second.second;
+					}
+					track[pari] = c1;
+					adj.erase(parj);
+					track.erase(parj);
+				}
+			
 		}
-	}
 
-	cout<<"Pass "<<pass<<": "<<modularity<<endl;
 	pass++;
+	if(modularity<modu)	modularity=modu;
+	else			break;
+	cout<<"Pass "<<pass<<": "<<modularity<<endl;
+	
+		
  }
-
+	cerr<<modularity<<endl;
  vector<int> cover(n,0);
 
- /*printing the vertices present in the final clusters after the algo has run, line by line*/
- 	for(unordered_map<int,cluster >::iterator it=track.begin();it!=track.end();it++)
+ 	
+/*printing the vertices present in the final clusters after the algo has run, line by line*/
+ 	for(map<int,cluster >::iterator it=track.begin();it!=track.end();it++)
 	{
 		i=it->first;
 		cluster c=track[i];
@@ -356,59 +368,62 @@ int main()
 	// Make directory 
 	if (system("mkdir clusters") != 0)
 	{
-		system("rm -rf clusters");
+		system("rmdir clusters /s");
+		system("Y");
 		system("mkdir clusters");
 	}
 	///////////////
 
-	freopen("./clusters/cover.txt","w",stdout);
+	freopen("C:\\Users\\HKD\\Lovain\\clusters\\cover.txt","w",stdout);
 	for(i=0;i<n;i++)
 	  cout<<i<<" "<<cover[i]<<endl;
 
-	for(unordered_map<int,cluster >::iterator it=track.begin();it!=track.end();it++)
+	for(map<int,cluster >::iterator it=track.begin();it!=track.end();it++)
 	{
-		i=it->first;
-		stringstream ss;	
-		ss << i;
-		string filename = "./clusters/" + ss.str();
-		filename.append(".txt");
-		freopen(filename.c_str(),"w",stdout);
-		cluster c=track[i];
-		int rctr,bctr;
-		rctr=bctr=0;
-		cout<<"Cluster "<<i<<": "<<endl;
-		cout<<"Vertices in this cluster : "<<c.vert.size()<<endl;
-		// fl = 1;
-		for(j=0;j<c.vert.size();j++)
+		if(colour[it->first]!=-1)
 		{
-			if(colour[c.vert[j]]==1)
+			i=it->first;
+			stringstream ss;	
+			ss << i;
+			string filename = "C:\\Users\\HKD\\Lovain\\clusters\\" + ss.str();
+			filename.append(".txt");
+			freopen(filename.c_str(),"w",stdout);
+			cluster c=track[i];
+			int rctr,bctr;
+			rctr=bctr=0;
+			cout<<"Cluster "<<i<<": "<<endl;
+			cout<<"Vertices in this cluster : "<<c.vert.size()<<endl;
+			// fl = 1;
+			for(j=0;j<c.vert.size();j++)
 			{
-				cout<<c.vert[j];
-				if(fl==1)
-					cout<<" "<<label[c.vert[j]]<<" -> Red"<<endl;
-				else
-					cout<<"-> Red"<<endl;	
-				rctr++;
-			}
-			else
-			{
+				if(colour[c.vert[j]]==1)
+				{
+					cout<<c.vert[j];
+					if(fl==1)
+						cout<<" "<<label[c.vert[j]]<<" -> Red"<<endl;
+					else
+						cout<<"-> Red"<<endl;	
+					rctr++;
+				}
+				else if(colour[c.vert[j]]==0)
+				{
+					
+					cout<<c.vert[j];
+					if(fl==1)
+						cout<<" "<<label[c.vert[j]]<<"-> Blue"<<endl;
+					else
+						cout<<"-> Blue"<<endl;
+					bctr++;
+				}
 				
-				cout<<c.vert[j];
-				if(fl==1)
-					cout<<" "<<label[c.vert[j]]<<"-> Blue"<<endl;
-				else
-					cout<<"-> Blue"<<endl;
-				bctr++;
 			}
-			
+			double dens= c.self*1.0;
+			if(rctr==0 || bctr==0)
+			  dens=0;
+			else
+			dens/=(rctr*bctr);
+			cout<<"Edge density of this cluster = "<<dens<<endl;
+			cout<<endl<<endl;
 		}
-		double dens= c.self*1.0;
-		if(rctr==0 || bctr==0)
-		  dens=0;
-		else
-		dens/=(rctr*bctr);
-		cout<<"Edge density of this cluster = "<<dens<<endl;
-		cout<<endl<<endl;
 	}
-
 }
